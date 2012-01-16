37bb147c274a98d6c6b510441e3106d9184c0965
Tonny Xu <tonny.xu@gmail.com>
2012-01-16 15:36:41 +0900

    Restored the test/product settings for DB Configuration.

--- app/Config/database.php
+++ app/Config/database.php
@@ -10,4 +10,25 @@ class DATABASE_CONFIG {
 		'database' => 'Holidays',
 		'encoding' => 'utf8'
 	);
+  /*
+   * Update the DB settings according to your server.
+   */
+	public $test = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'Holidays',
+		'encoding' => 'utf8'
+	);
+	public $product = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'Holidays',
+		'encoding' => 'utf8'
+	);
 }
